6e9e492add430dee0ce1d635e66de6454703fe73
aung_myat <gusion762389@gmail.com>
2026-01-09 11:35:14 +0630

    add works page content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import type { ComponentPropsWithoutRef, FC, ReactNode } from "react";
+import type { FC, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
