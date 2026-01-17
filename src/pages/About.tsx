1d02f01133eb6b26d2c0a71186062a725ed84e03
aung_myat <gusion762389@gmail.com>
2026-01-17 17:18:02 +0630

    fix svg path

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,15 @@ import type { FC } from "react";
 import Section from "../components/Section";
 import type { PageType } from "../App";
 import CopyText from "../components/CopyText";
+import heroSvg from "../assets/hero.svg";
 
 const About: FC<PageType> = ({ setActive }) => {
     return (
-        <Section setActive={setActive} className="pt-20 px-3" id="about">
+        <Section
+            style={{ backgroundImage: `url(${heroSvg})` }}
+            setActive={setActive}
+            className="pt-20 px-3"
+            id="about">
             <div className=" mb-3 font-bold text-md">
                 Hi, My name is{" "}
                 <div className="text-2xl font-mono">Aung Myat Htut </div>
